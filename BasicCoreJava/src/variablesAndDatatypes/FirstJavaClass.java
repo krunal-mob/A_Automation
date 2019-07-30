b7781fb6c5d598585f50810557ea7a97417ceba4
Abhishek Sarkar <Sarkar.abhjishek06@gmail.com>
2019-07-31 02:08:06 +0530

    I am committing changes

---
+++ BasicCoreJava/src/variablesAndDatatypes/FirstJavaClass.java
@@ -0,0 +1,41 @@
+package variablesAndDatatypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Selenium");
+		
+		System.out.println("Hello Selenium");
+		
+		
+		int age = 23; // byte , short , int , long 
+		char gender = 'M';
+		String name = "ABhishek";
+		double marks = 22.33;
+		
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(name);
+		System.out.println(marks);
+		
+		System.out.println("I am making changes");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ BasicCoreJava/src/variablesAndDatatypes/SecondJavaClass.java
@@ -0,0 +1,25 @@
+package variablesAndDatatypes;
+
+public class SecondJavaClass {
+
+	public static void main(String[] args) {
+		
+		//Arithmetic Operators
+		
+		int a = 10;
+		int b = 20;
+		String ab="Abhishek";
+		
+		
+		
+		
+		System.out.println(a + b);
+		System.out.println(ab);
+		
+		
+	
+		
+
+	}
+
+}
