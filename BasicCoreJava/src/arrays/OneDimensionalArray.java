53d5dab7bfb4f889f34e1cbf175bcd7af640cfeb
Abhishek Sarkar <Sarkar.abhjishek06@gmail.com>
2019-08-05 15:08:00 +0530

    5th August

--- BasicCoreJava/src/arrays/OneDimensionalArray.java
+++ BasicCoreJava/src/arrays/OneDimensionalArray.java
@@ -5,7 +5,7 @@ public class OneDimensionalArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] str = new String[7];
+		String[] str = new String[7]; // First way of creating Array
 		str[0] = "Abhishek";
 		str[1] = "Abhay";
 		str[2] = "Suraj";
@@ -21,7 +21,21 @@ public class OneDimensionalArray {
 		}
 		
 		
-
+		
+		
+		int phone[] = new int[7];  // Second way of creating array 
+		
+		phone[0]=1234;
+		
+		System.out.println(phone[0]);
+		
+		
+		int [] size = {10,20,30 ,40}; // Third way of creating array
+		
+		for (int i=0;i<size.length;i++) {
+			
+			System.out.println(size[i]);
+		}
 
 	}
 
