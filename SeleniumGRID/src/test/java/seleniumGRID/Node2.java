63c753018af03808140ba97e9a70b631fc6be3c4
Abhishek Sarkar <Sarkar.abhjishek06@gmail.com>
2019-12-25 21:52:25 +0530

    25th december 2019

--- SeleniumGRID/src/test/java/seleniumGRID/Node2.java
+++ SeleniumGRID/src/test/java/seleniumGRID/Node2.java
@@ -1,56 +1,49 @@
 package seleniumGRID;
 
-import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class Node1 {
-
-
-
+public class Node2 {
+	
+	
 	@Test
-	public void testMethod() throws MalformedURLException {
-
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
+	public void testMethodNode2() throws MalformedURLException {
+		
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
 
 		//Define Desired capabilities
-		cap.setBrowserName("chrome");
+		cap.setBrowserName("firefox");
 		cap.setPlatform(Platform.MAC);
 
 		//Define Chrome Options --> Recently introduced for chrome
-		ChromeOptions options = new ChromeOptions();
-		options.merge(cap);
+		//ChromeOptions options = new ChromeOptions();
+		//options.merge(cap);
 
 
 		String hubURL= "http://192.168.0.103:4444/wd/hub"; //Clients should connect to http://192.168.0.103:4444/wd/hub
 
-		WebDriver driver = new RemoteWebDriver(new URL(hubURL), options); //can use cap also instead of options
+		WebDriver driver = new RemoteWebDriver(new URL(hubURL), cap); //can use cap also instead of options
    
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		
-		driver.get("https://www.guru99.com/introduction-to-selenium-grid.html#5");
+		driver.get("https://www.youtube.com/");
 		
 		System.out.println(driver.getTitle());
-
-
+		
+		
+		
+		
 	}
-
-
-
-
-
-
-
 
 }
